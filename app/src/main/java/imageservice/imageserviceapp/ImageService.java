dec5f11242bf8728216bb2a6321c55b7e448a35b
Uri Ronen <ronenuri@gmail.com>
2018-06-17 12:49:14 +0300

    Added wifi broadcaster

--- app/src/main/java/imageservice/imageserviceapp/ImageService.java
+++ app/src/main/java/imageservice/imageserviceapp/ImageService.java
@@ -1,7 +1,13 @@
 package imageservice.imageserviceapp;
 
 import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiManager;
 import android.os.Environment;
 import android.os.IBinder;
 import android.widget.Toast;
@@ -10,32 +16,53 @@ import java.io.File;
 
 public class ImageService extends Service {
 
+    private BroadcastReceiver receiver;
+    private final IntentFilter filter = new IntentFilter();
+
     @Override
     public void onCreate() {
         super.onCreate();
-        // Put service code here
-        File dcim =
-                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM);
-        if (dcim == null) {
-            return;
-        }
+        // Getting a file list of our cameras photos
+        File dcim = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM);
         File[] pics = dcim.listFiles();
-
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Toast.makeText(this, "Service Started!", Toast.LENGTH_LONG).show();
+        filter.addAction("android.net.wifi.supplicant.CONNECTION_CHANGE");
+        filter.addAction("android.net.wifi.STATE_CHANGE");
+        this.receiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                WifiManager wifiManager = (WifiManager) context.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+                NetworkInfo networkInfo = intent.getParcelableExtra(WifiManager.EXTRA_NETWORK_INFO);
+                if (networkInfo != null) {
+                    if (networkInfo.getType() == ConnectivityManager.TYPE_WIFI) {
+                        if (networkInfo.getState() == NetworkInfo.State.CONNECTED) {
+                            startTransfer();
+                        }
+                    }
+                }
+            }
+        };
+        this.registerReceiver(this.receiver, this.filter);
         return START_STICKY;
+    }
+
+    private void startTransfer() {
+        Toast.makeText(this,"Wi-fi connected.\n Transferring photos...", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onDestroy() {
-        Toast.makeText(this,"Service Stopped :(", Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "Service Stopped :(", Toast.LENGTH_LONG).show();
     }
 
     @Override
     public IBinder onBind(Intent intent) {
         return null;
     }
+
+
 }
